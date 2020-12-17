b16168040646a77ab2b1dd48b6d36a22e45994dc
Robert Frei <rfrei@nd.edu>
2020-12-17 14:16:43 -0500

    Used black on measure

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,12 +4,13 @@ This module is for functions which perform measurements
 
 import numpy as np
 
+
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
@@ -25,12 +26,12 @@ def calculate_distance(rA, rB):
     ----------
     rA, rB : np.ndarray
         The coordinate of each point
-    
+
     Returns
     -------
     dist : float
         The distance between the two points
-    
+
     Examples
     --------
     >>> r1 = np.array([0,0,0])
@@ -39,6 +40,6 @@ def calculate_distance(rA, rB):
     1.0
     """
     # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
+    d = rA - rB
+    dist = np.linalg.norm(d)
     return dist
